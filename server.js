867aec8d43d2c787ac1703cba17730ae1183695e
SanB <sandro.brignoli@outlook.fr>
2021-11-04 12:16:38 +0100

    ✨ Initialize Mongoose

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 import dotenv from "dotenv";
 import express from "express";
+import mongoose from "mongoose";
 import cors from "cors";
 
 dotenv.config();
@@ -8,8 +9,20 @@ dotenv.config();
 const PORT = process.env.PORT || 5000;
 const app = express();
 app.listen(PORT, () => {
-  console.log(`Express Server started on PORT ${PORT}`);
+  console.log(`Express server started on PORT ${PORT}`);
 });
+
+// Connect Database
+const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGODB);
+    console.log("DATABASE CONNECTED");
+  } catch (err) {
+    console.log(err.message);
+    process.exit(1);
+  }
+};
+connectDB();
 
 // Init Middleware
 app.use(express.json());
