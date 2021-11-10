c4b9b14637f6b1053b1552085965e3c62f5f2cff
SanB <sandro.brignoli@outlook.fr>
2021-11-10 09:32:28 +0100

    ✨ Create PDF Card PUT Route

--- routes/api/pdfCards.js
+++ routes/api/pdfCards.js
@@ -61,6 +61,58 @@ router.get("/:block", async (req, res) => {
   }
 });
 
+// @route   PUT api/pdfCards/:id
+// @desc    Edit a PDFCard
+// @access  Private
+router.put(
+  "/:id",
+  [
+    auth,
+    [
+      check("title", "Veuillez donner un titre").not().isEmpty(),
+      check("PDF", "Veuillez importer un fichier PDF").not().isEmpty(),
+      check("block", "Veuillez choisir une fenêtre où la carte apparaîtra")
+        .not()
+        .isEmpty(),
+    ],
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    const { title, PDF, block } = req.body;
+    const pdfCardsFields = {};
+    pdfCardsFields.title = title;
+    pdfCardsFields.PDF = PDF;
+    pdfCardsFields.block = block;
+    if (title) pdfCardsFields.title = title;
+    if (PDF) pdfCardsFields.PDF = PDF;
+    if (block) pdfCardsFields.block = block;
+
+    try {
+      const pdfCard = await PDFCard.findOneAndUpdate(
+        { _id: req.params.id },
+        { $set: pdfCardsFields },
+        { new: true }
+      );
+      if (!pdfCard) {
+        return res.status(404).json({
+          errors: [{ msg: "Carte PDF introuvable" }],
+        });
+      }
+      await pdfCard.save();
+      res.json({ msg: "Les modifications ont bien été enregistrées" });
+    } catch (err) {
+      console.error(err.message);
+      if (err.kind == "ObjectId") {
+        return res.status(404).json({ msg: "Carte PDF introuvable" });
+      }
+      res.status(500).send("Server Error");
+    }
+  }
+);
+
 // @route   DELETE api/pdfCards/:id
 // @desc    Delete a PDFCard
 // @access  Private
