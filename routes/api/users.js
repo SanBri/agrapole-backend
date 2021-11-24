ca90044e20d35fe8800bd668701b75e66754f936
SanB <sandro.brignoli@outlook.fr>
2021-11-24 14:28:40 +0100

    ✨ Create Change Mail Route

--- routes/api/users.js
+++ routes/api/users.js
@@ -142,4 +142,47 @@ router.put(
   }
 );
 
+// @route   PUT api/users/mail/:id
+// @desc    Change mail
+// @access  Private
+router.put(
+  "/mail/:id",
+  [
+    auth,
+    [
+      check("mail", "Veuillez entrer votre mot de passe").not().isEmpty(),
+      check("password", "Veuillez entrer votre mot de passe").not().isEmpty(),
+    ],
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    const { mail, password } = req.body;
+    try {
+      const isMatch = checkPassword(req.params.id, password);
+
+      isMatch.then(async (result) => {
+        if (result === true) {
+          let user = await User.findOneAndUpdate(
+            { _id: req.params.id },
+            { $set: { mail: mail } },
+            { new: true }
+          );
+          await user.save();
+          res.json({ msg: "L'adresse mail a bien été modifiée" });
+        } else {
+          return res.status(400).json({
+            errors: [{ msg: result }],
+          });
+        }
+      });
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send("Server Error");
+    }
+  }
+);
+
 export default router;
