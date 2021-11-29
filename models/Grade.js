aa466d829dc8e6cb03709ceab8ff89ce006a4b6c
SanB <sandro.brignoli@outlook.fr>
2021-11-29 14:04:43 +0100

    ✨ Create Grade Model

---
+++ models/Grade.js
@@ -0,0 +1,25 @@
+import mongoose from "mongoose";
+
+const GradeSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    unique: true,
+    default: "grade",
+  },
+  title: {
+    type: String,
+    required: true,
+  },
+  average: {
+    type: Number,
+    required: true,
+  },
+  scale: {
+    type: Number,
+    required: true,
+  },
+});
+
+const Grade = mongoose.model("Grade", GradeSchema);
+export default Grade;
